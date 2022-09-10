4c05c823400681c956bf8cb8b07712e0d8c59292
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2022-09-10 17:36:15 +0900

    대충 프로젝트 설정
    - 프로젝트 설정 너무 오래돼서 기억이 안남....
    - 대충 씬 만들고 씬 이동 구현

---
+++ minigame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // ¾À ÀüÈ¯ À§ÇÔ
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int idx)
+    {
+        switch (idx)
+        {
+            case 1:
+                SceneManager.LoadScene("Stage1");
+                break;
+            case 2:
+                SceneManager.LoadScene("Stage2");
+                break;
+            case 3:
+                SceneManager.LoadScene("Stage3");
+                break;
+            case 4:
+                SceneManager.LoadScene("Stage4");
+                break;
+        }
+        
+    }
+}
