dc400f46fb3b287a2a316a5d1c69fe6570226ae2
wooyeon <bethelight730@gmail.com>
2022-09-28 17:04:36 +0900

    .

--- minigame/Assets/Scripts/Stage1_Script/Grappler.cs
+++ minigame/Assets/Scripts/Stage1_Script/Grappler.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
+    //참고 영상
+    //https://www.youtube.com/watch?v=P-UscoFwaE4
+
     public Camera mainCamera;
     public LineRenderer lineRenderer;
     public DistanceJoint2D distanceJoint;
+
+    //public float x_pos;
 
     void Start()
     {
@@ -17,9 +22,13 @@ public class Grappler : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            // linePos : 줄 위치는 천장 가운데에
+            //Vector2 linePos = new Vector2(0f, 4.5f);
             Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
             lineRenderer.SetPosition(0, mousePos);
             lineRenderer.SetPosition(1, transform.position);
+
             distanceJoint.connectedAnchor = mousePos;
             distanceJoint.enabled = true;
             lineRenderer.enabled = true;
@@ -30,7 +39,7 @@ public class Grappler : MonoBehaviour
             lineRenderer.enabled = false;
         }
 
-        if(distanceJoint.enabled)
+        if (distanceJoint.enabled)
         {
             lineRenderer.SetPosition(1, transform.position);
         }
