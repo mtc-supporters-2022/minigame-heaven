e1c4b6eb3cb9b2c49fc91590d20f25428468f872
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2022-10-05 01:18:04 +0900

    줄 타는 공주

--- minigame/Assets/Scripts/Stage1_Script/CameraMove.cs
+++ minigame/Assets/Scripts/Stage1_Script/CameraMove.cs
@@ -5,35 +5,15 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public float cameraSpeed = 20.0f;
-
     public GameObject player;
     private void Update()
     {
-        Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        // targetPos 대신 player.transform.position을 사용했더니 플레이어의 z축 위치까지 따라가서 실행시키면 안보이는 오류가 났었음
+        Vector3 targetPos = new Vector3(player.transform.position.x + 0.7f, player.transform.position.y, transform.position.z);
+
+        // 선형보간 Lerp
+        // 근데 왜케 덜덜거리지
+        transform.position = Vector3.Lerp(this.transform.position, targetPos, Time.deltaTime * cameraSpeed);
+        // transform.position ㄱㄴ, this.transform.position ㄱㄴ
     }
-    /*
-    public GameObject Target;               // 카메라가 따라다닐 타겟
-
-    public float offsetX = 0.0f;            // 카메라의 x좌표
-    public float offsetY = 0.0f;           // 카메라의 y좌표
-    public float offsetZ = 0.0f;          // 카메라의 z좌표
-
-    public float CameraSpeed = 10.0f;       // 카메라의 속도
-    Vector3 TargetPos;                      // 타겟의 위치
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        // 타겟의 x, y, z 좌표에 카메라의 좌표를 더하여 카메라의 위치를 결정
-        TargetPos = new Vector3(
-            Target.transform.position.x + offsetX,
-            Target.transform.position.y + offsetY,
-            Target.transform.position.z + offsetZ
-            );
-
-        // 카메라의 움직임을 부드럽게 하는 함수(Lerp)
-        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
-    }*/
 }
