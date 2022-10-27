bd8a7527f98e146763de69f9edcb049b53a5eccb
Sangwook123 <operawook@catholic.ac.kr>
2022-10-28 04:43:32 +0900

    .

--- minigame/Assets/playerMove.cs
+++ minigame/Assets/playerMove.cs
@@ -5,38 +5,51 @@ using UnityEngine;
 public class playerMove : MonoBehaviour
 {
     int posx = 0;
+    int but = 0;
     void Update()
     {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0);
+
         if (posx > -2)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+            if (Input.GetMouseButtonDown(0) == true && mousePos.x<360)
             {
                 posx = posx - 1;
             }
         }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        if (Input.GetMouseButton(0) == true && mousePos.x < 360)
         {
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(posx,-4,0), 0.05f);
 
         }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) == true)
+        if (Input.GetMouseButtonUp(0) == true && mousePos.x < 360)
         {
             gameObject.transform.position = (new Vector3(posx,-4 , 0));
         }
         if (posx < 2)
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+            if (Input.GetMouseButtonDown(0) == true && mousePos.x > 360)
             {
                 posx = posx + 1;
             }
         }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
+        if (Input.GetMouseButton(0) == true && mousePos.x > 360)
         {
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(posx,-4,0), 0.05f);
         }
-        if (Input.GetKeyUp(KeyCode.RightArrow) == true)
+        if (Input.GetMouseButtonUp(0) == true && mousePos.x > 360)
         {
             gameObject.transform.position = (new Vector3(posx,-4 , 0));
         }
+    }
+
+    public void LButtonDown()
+    {
+        but = 1;
+    }
+
+    public void RButtonDown()
+    {
+        but = 2;
     }
 }
