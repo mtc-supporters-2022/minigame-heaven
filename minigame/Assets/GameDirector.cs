297d7d467c6746fd67db3a20aa6784c0b8124e4a
Sangwook123 <operawook@catholic.ac.kr>
2022-10-28 05:17:30 +0900

    마우스클릭 공주 애니메이션

--- minigame/Assets/GameDirector.cs
+++ minigame/Assets/GameDirector.cs
@@ -38,66 +38,66 @@ public class GameDirector : MonoBehaviour
             this.pTime = 0;
             this.point += 10*level;
         }
-        if(this.time > 0 && this.time <= 10)
+        if(this.time > 0 && this.time <= 3)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -7.0f);
             this.level = 1;
-        }else if(this.time > 10 && this.time <= 20)
+        }else if(this.time > 3 && this.time <= 6)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -7.5f);
             this.level = 2;
         }
-        else if (this.time > 20 && this.time <= 30)
+        else if (this.time > 6 && this.time <= 9)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.95f, -8f);
             this.level = 3;
         }
-        else if (this.time > 30 && this.time <= 40)
+        else if (this.time > 9 && this.time <= 12)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.95f, -8.5f);
             this.level = 4;
         }
-        else if (this.time > 40 && this.time <= 50) 
+        else if (this.time > 12 && this.time <= 15) 
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -9f);
             this.level = 5;
         }
-        else if (this.time > 50 && this.time <= 60)
+        else if (this.time > 15 && this.time <= 18)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -9.5f);
             this.level = 6;
         }
-        else if (this.time > 60 && this.time <= 70)
+        else if (this.time > 18 && this.time <= 21)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.85f, -10f);
             this.level = 7;
         }
-        else if (this.time > 70 && this.time <= 80)
+        else if (this.time > 21 && this.time <= 24)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.85f, -10.5f);
             this.level = 8;
         }
-        else if (this.time > 80 && this.time <= 100)
+        else if (this.time > 24 && this.time <= 27)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -11f);
             this.level = 9;
         }
-        else if (this.time > 100 && this.time <= 120)
+        else if (this.time > 27 && this.time <= 30)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.75f, -13f);
             this.level = 10;
         }
-        else if (this.time > 120 && this.time <= 140)
+        else if (this.time > 30 && this.time <= 33)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -15f);
             this.level = 11;
         }
-        else if (this.time > 140 && this.time <= 160)
+        else if (this.time > 33 && this.time <= 36)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.6f, -17f);
             this.level = 12;
         }
-        else if (this.time > 160)
+        else if (this.time > 36)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.5f, -19f);
             this.level = 13;
--- minigame/Assets/playerMove.cs
+++ minigame/Assets/playerMove.cs
@@ -5,51 +5,42 @@ using UnityEngine;
 public class playerMove : MonoBehaviour
 {
     int posx = 0;
-    int but = 0;
+
     void Update()
     {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0);
 
         if (posx > -2)
         {
-            if (Input.GetMouseButtonDown(0) == true && mousePos.x<360)
+            if (Input.GetMouseButtonDown(0) == true && mousePos.x<720)
             {
                 posx = posx - 1;
             }
         }
-        if (Input.GetMouseButton(0) == true && mousePos.x < 360)
+        if (Input.GetMouseButton(0) == true && mousePos.x < 720)
         {
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(posx,-4,0), 0.05f);
 
         }
-        if (Input.GetMouseButtonUp(0) == true && mousePos.x < 360)
+        if (Input.GetMouseButtonUp(0) == true && mousePos.x < 720)
         {
             gameObject.transform.position = (new Vector3(posx,-4 , 0));
         }
         if (posx < 2)
         {
-            if (Input.GetMouseButtonDown(0) == true && mousePos.x > 360)
+            if (Input.GetMouseButtonDown(0) == true && mousePos.x > 720)
             {
                 posx = posx + 1;
             }
         }
-        if (Input.GetMouseButton(0) == true && mousePos.x > 360)
+        if (Input.GetMouseButton(0) == true && mousePos.x > 720)
         {
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(posx,-4,0), 0.05f);
         }
-        if (Input.GetMouseButtonUp(0) == true && mousePos.x > 360)
+        if (Input.GetMouseButtonUp(0) == true && mousePos.x > 720)
         {
             gameObject.transform.position = (new Vector3(posx,-4 , 0));
         }
     }
 
-    public void LButtonDown()
-    {
-        but = 1;
-    }
-
-    public void RButtonDown()
-    {
-        but = 2;
-    }
 }
