a3bf764525153af88f5bb31b7fa63f0986da9bc0
Sangwook123 <operawook@catholic.ac.kr>
2022-10-31 03:39:37 +0900

    95

--- minigame/Assets/Scripts/Stage4/GameDirector.cs
+++ minigame/Assets/Scripts/Stage4/GameDirector.cs
@@ -38,66 +38,66 @@ public class GameDirector : MonoBehaviour
             this.pTime = 0;
             this.point += 10*level;
         }
-        if(this.time > 0 && this.time <= 5)
+        if(this.time > 0 && this.time <= 10)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -7.0f);
             this.level = 1;
-        }else if(this.time > 5 && this.time <= 10)
+        }else if(this.time > 10 && this.time <= 20)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -7.5f);
             this.level = 2;
         }
-        else if (this.time > 10 && this.time <= 15)
+        else if (this.time > 20 && this.time <= 30)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.95f, -8f);
             this.level = 3;
         }
-        else if (this.time > 15 && this.time <= 20)
+        else if (this.time > 30 && this.time <= 40)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.95f, -8.5f);
             this.level = 4;
         }
-        else if (this.time > 20 && this.time <= 25) 
+        else if (this.time > 40 && this.time <= 50) 
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -9f);
             this.level = 5;
         }
-        else if (this.time > 25 && this.time <= 30)
+        else if (this.time > 50 && this.time <= 60)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -9.5f);
             this.level = 6;
         }
-        else if (this.time > 30 && this.time <= 35)
+        else if (this.time > 60 && this.time <= 70)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.85f, -10f);
             this.level = 7;
         }
-        else if (this.time > 35 && this.time <= 40)
+        else if (this.time > 70 && this.time <= 80)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.85f, -10.5f);
             this.level = 8;
         }
-        else if (this.time > 40 && this.time <= 45)
+        else if (this.time > 80 && this.time <= 90)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -11f);
             this.level = 9;
         }
-        else if (this.time > 45 && this.time <= 50)
+        else if (this.time > 90 && this.time <= 100)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.75f, -13f);
             this.level = 10;
         }
-        else if (this.time > 50 && this.time <= 55)
+        else if (this.time > 100 && this.time <= 110)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -15f);
             this.level = 11;
         }
-        else if (this.time > 55 && this.time <= 60)
+        else if (this.time > 110 && this.time <= 120)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.6f, -17f);
             this.level = 12;
         }
-        else if (this.time > 60)
+        else if (this.time > 120)
         {
             this.generator.GetComponent<ItemGenerator>().SetParameter(0.5f, -19f);
             this.level = 13;
