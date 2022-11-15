f4dd33c3764daa0272135d025d902d03597b585b
wooyeon <bethelight730@gmail.com>
2022-11-15 23:49:58 +0900

    무한배경 완성

--- minigame/Assets/Scripts/Stage3/GenerateBG.cs
+++ minigame/Assets/Scripts/Stage3/GenerateBG.cs
@@ -14,11 +14,22 @@ public class GenerateBG : MonoBehaviour
     int c_num = 0; // 가장 왼쪽에 있는 친구들
     int w_num = 0;
     int l_num = 0;
-    public Vector3[] startPos;
+    Vector3[] startPos = new Vector3[10];
 
     void Start()
     {
-        Debug.Log(player.transform.position);
+        startPos[0] = ceiling[0].transform.position;
+        startPos[1] = ceiling[1].transform.position;
+        startPos[2] = ceiling[2].transform.position;
+
+        startPos[3] = wall[0].transform.position;
+        startPos[4] = wall[1].transform.position;
+        startPos[5] = wall[2].transform.position;
+        startPos[6] = wall[3].transform.position;
+
+        startPos[7] = lava[0].transform.position;
+        startPos[8] = lava[1].transform.position;
+        startPos[9] = lava[2].transform.position;
     }
 
 
@@ -40,16 +51,16 @@ public class GenerateBG : MonoBehaviour
             }
 
             // 벽 판별 후 이동
-            if (player.transform.position.x > wall[(w_num + 2) % 4].transform.position.x)
+            if (player.transform.position.x > wall[(w_num + 3) % 4].transform.position.x)
             {
-                wall[w_num].transform.position = new Vector3(wall[(w_num + 2) % 4].transform.position.x + 7.92f, wall[w_num].transform.position.y, wall[w_num].transform.position.z);
+                wall[w_num].transform.position = new Vector3(wall[(w_num + 3) % 4].transform.position.x + 7.92f, wall[w_num].transform.position.y, wall[w_num].transform.position.z);
                 w_num = (w_num + 1) % 4; // w_num 갱신
             }
 
             // 용암 판별 후 이동
             if (player.transform.position.x > lava[(l_num + 2) % 3].transform.position.x)
             {
-                lava[l_num].transform.position = new Vector3(lava[(c_num + 2) % 3].transform.position.x + 7.8f, lava[l_num].transform.position.y, lava[l_num].transform.position.z);
+                lava[l_num].transform.position = new Vector3(lava[(l_num + 2) % 3].transform.position.x + 7.8f, lava[l_num].transform.position.y, lava[l_num].transform.position.z);
                 l_num = (l_num + 1) % 3; // l_num 갱신
             }
         }
@@ -60,18 +71,18 @@ public class GenerateBG : MonoBehaviour
             l_num = 0;
 
             // 원위치
-            startPos[0].x = ceiling[0].transform.position.x;
-            startPos[1].x = ceiling[1].transform.position.x;
-            startPos[2].x = ceiling[2].transform.position.x;
+            ceiling[0].transform.position = startPos[0];
+            ceiling[1].transform.position = startPos[1];
+            ceiling[2].transform.position = startPos[2];
 
-            startPos[3].x = wall[0].transform.position.x;
-            startPos[4].x = wall[1].transform.position.x;
-            startPos[5].x = wall[2].transform.position.x;
-            startPos[6].x = wall[3].transform.position.x;
+            wall[0].transform.position = startPos[3];
+            wall[1].transform.position = startPos[4];
+            wall[2].transform.position = startPos[5];
+            wall[3].transform.position = startPos[6];
 
-            startPos[7].x = lava[0].transform.position.x;
-            startPos[8].x = lava[1].transform.position.x;
-            startPos[9].x = lava[2].transform.position.x;
+            lava[0].transform.position = startPos[7];
+            lava[1].transform.position = startPos[8];
+            lava[2].transform.position = startPos[9];
         }
 
     }
