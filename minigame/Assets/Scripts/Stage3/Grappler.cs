3020c72cc4e0f15ca3c052e32ca705eccc487571
wooyeon <bethelight730@gmail.com>
2022-11-15 15:07:11 +0900

    1115 우연 1회차
    일시정지 시 정지버튼 비활 ㅇ
    공주 시작 위치 정하기 ㅇ
    스코어 올라가게 ㅇ
    베스트 스코어 추가 ㅇ
    일시정지 시 줄 안생기게 ㅇ
    1080 1920에 맞게 변경

--- minigame/Assets/Scripts/Stage3/Grappler.cs
+++ minigame/Assets/Scripts/Stage3/Grappler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Grappler : MonoBehaviour
 {
@@ -14,8 +15,15 @@ public class Grappler : MonoBehaviour
     public Rigidbody2D rigidbody2d;
 
     [Header("GameObject")]
+    public GameObject UIManager;
+
     public GameObject startPnl;
     public GameObject gameoverPnl;
+    
+    float time;
+    int score;
+    public Text scoreTxt;
+    public Text bscoreTxt;
 
     bool isGaming;
     Vector3 bestDist;
@@ -34,6 +42,9 @@ public class Grappler : MonoBehaviour
     {
         if (isGaming)
         {
+            time += Time.deltaTime;
+            score = (int)time * 10;
+            scoreTxt.text = score + "point";
 
             if (bestDist.x < transform.position.x) // 멀리 간 거리 갱신
                 bestDist.x = transform.position.x;
@@ -50,7 +61,7 @@ public class Grappler : MonoBehaviour
             temp.y += 0.3f * Time.deltaTime;
             transform.position = temp;
 
-            if (Input.GetKeyDown(KeyCode.Mouse0)) // 마우스 눌렀을 때
+            if (Input.GetKeyDown(KeyCode.Mouse0) && !UIManager.GetComponent<UIManager>().isPause) // 마우스 눌렀을 때
             {
                 // 줄 생성 위치
                 Vector3 linePos = new Vector3(transform.position.x + 3f, 4.5f, 0f);
@@ -106,8 +117,16 @@ public class Grappler : MonoBehaviour
         {
             gameoverPnl.SetActive(true);
             rigidbody2d.bodyType = RigidbodyType2D.Static;
-            transform.position = new Vector3(-1.2243f, 0.6691437f, -0.22938f);
+            transform.position = new Vector3(-0.31f, 1.23f, -1f);
             bestDist = transform.position;
+
+            if (score > GameManager.Instance.bestScore[3])
+                GameManager.Instance.bestScore[3] = score;
+            bscoreTxt.text = "best score: " + GameManager.Instance.bestScore[3].ToString() + "\nnow score: " + score;
+
+            time = 0;
+            score = 0;
+            scoreTxt.text = "0point";
         }
     }
 }
--- minigame/Assets/Scripts/Stage3/UIManager.cs
+++ minigame/Assets/Scripts/Stage3/UIManager.cs
@@ -6,32 +6,27 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [Header("GameObject")]
+    public GameObject pauseBtn;
     public GameObject pausePnl;
     public GameObject gameoverPnl;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isPause;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void pauseBtn(bool pause)
+    public void Pause(bool pause)
     {
         if (pause)
         {
+            isPause = true;
             pausePnl.SetActive(true);
             Time.timeScale = 0;
+            pauseBtn.GetComponent<Button>().interactable = false;
         }
         else
         {
+            isPause = false;
             pausePnl.SetActive(false);
             Time.timeScale = 1;
+            pauseBtn.GetComponent<Button>().interactable = true;
         }
     }
 }
